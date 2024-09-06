388c9ca7a8db1054531e4e4735c021cddb873a30
ufuomaisaac <ejemudaroufuoma@gmail.com>
2024-09-06 20:54:00 +0100

    added baseViewModel

---
+++ app/src/main/java/com/example/clientmanager/core/BaseViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.clientmanager.core
+
+import androidx.lifecycle.ViewModel
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+
+open class BaseViewModel: ViewModel() {
+
+    protected val disposables = CompositeDisposable()
+
+    override fun onCleared() {
+        disposables.dispose()
+        super.onCleared()
+    }
+}
