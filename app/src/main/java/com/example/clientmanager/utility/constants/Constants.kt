309cab9082d1d91bee989d0e35d6974efd284bec
ufuomaisaac <ejemudaroufuoma@gmail.com>
2024-10-03 13:29:23 +0100

    code refactor

--- app/src/main/java/com/example/clientmanager/utility/constants/Constants.kt
+++ app/src/main/java/com/example/clientmanager/utility/constants/Constants.kt
@@ -1,6 +1,7 @@
 package com.example.clientmanager.utility.constants
 
 object Constants {
+    const val DEFAULT_ERROR_MESSAGE = "An error occurred. Please try again."
 
     const val CLIENT_MANAGER_BASE_URL = "https://cents-0ium.onrender.com/"
 
